e69b1623d0d5327fdccbaf7cc70361990d4dbb1f
fkaraagac11 <fehmi11.qa@gmail.com>
2020-06-30 22:54:44 -0400

    updated

---
+++ demotest.js
@@ -0,0 +1,10 @@
+const mocha = require("mocha");
+const assert = require("assert");
+//describe test
+describe("some demo tests", () => {
+    // create tests
+
+    it("add two numbers together", () => {
+        assert(2 + 3 === 5);
+    });
+});
